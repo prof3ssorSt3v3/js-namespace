af55040615ccf500ac54002dc5962f41582c867a
Steve Griffith <griffis@algonquincollege.com>
2017-10-07 09:35:12 -0400

    Added user list to main.js
    added all the student usernames

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 //using var will add the variable to the window object as a global variable
 var STEVE = {
-    users: ['prof3ssorSt3v3', 'abcd0001'],
+    users: ['prof3ssorSt3v3', 'loke0019', 'bari0053', 'boos0010',
+'corm0104','croz0034',
+'dala0016','dall0078',
+'guze0001','husa0011',
+'hyde0038','iusi0001',
+'juar0008','lath0028',
+'liu00415','liu00396',
+'mafe0002','maga0030',
+'mccr0124','meig0011',
+'morr0560','mudr0003',
+'nieu0005','raj00008',
+'sama0059','tanv0001',
+'tibu0003','vira0016',
+'will0684','ball0178',
+'blai0264','bouz0015',
+'cefa0007','chha0019',
+'curr0250','doan0028',
+'dorv0021','fran0436',
+'gong0030','guer0157',
+'he000060','ibra0206',
+'jalo0011','jele0006',
+'kini0004','liu00414',
+'liu00420','mcca0614',
+'nola0123','rafi0005',
+'reev0036','roy00427',
+'sant0178','shir0058,'
+'sing0961','son00020','stam0123'],
     init: function(){
         //check for querystring and load the single script OR load all scripts using users array
         let qs = location.search;
